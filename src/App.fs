49d2acee143cb7b8d090262155cfeb789ae29e21
David Neuy <quantjump@gmail.com>
2020-12-21 14:28:52 +0100

    Add Feliz (based on F# and Fable) and material-ui.

--- src/App.fs
+++ src/App.fs
@@ -2,6 +2,9 @@ module App
 
 open Feliz
 open Elmish
+//open Zanaptak.TypedCssClasses
+open Feliz.MaterialUI
+
 
 type State = { Count: int }
 
@@ -16,17 +19,86 @@ let update (msg: Msg) (state: State) =
     | Increment -> { state with Count = state.Count + 1 }, Cmd.none
     | Decrement -> { state with Count = state.Count - 1 }, Cmd.none
 
-let render (state: State) (dispatch: Msg -> unit) =
-    Html.div [
-        Html.button [
-            prop.onClick (fun _ -> dispatch Increment)
-            prop.text "Increment"
-        ]
+// Themes documentation links
+// - https://cmeeren.github.io/Feliz.MaterialUI/#usage/themes
+// - https://material-ui.com/customization/theming/
+let defaultTheme = Styles.createMuiTheme()
 
-        Html.button [
-            prop.onClick (fun _ -> dispatch Decrement)
-            prop.text "Decrement"
-        ]
+let darkTheme = Styles.createMuiTheme([
+  theme.palette.type'.dark
+  theme.palette.primary Colors.lightBlue
+  theme.palette.secondary Colors.pink
+  theme.palette.background.default' defaultTheme.palette.grey.``900``
+  theme.typography.h1.fontSize "3rem"
+  theme.typography.h2.fontSize "2rem"
+  theme.typography.h3.fontSize "1.5rem"
 
-        Html.h1 state.Count
+  theme.overrides.muiAppBar.colorDefault [
+    style.backgroundColor defaultTheme.palette.grey.A400
+  ]
+  theme.overrides.muiPaper.root [
+    style.backgroundColor defaultTheme.palette.grey.A400
+  ]
+  theme.overrides.muiDrawer.paper [
+    style.backgroundColor defaultTheme.palette.grey.``900``
+  ]
+
+  theme.props.muiAppBar [
+    appBar.color.default'
+  ]
+])
+
+// Styles documentation links
+// - https://github.com/cmeeren/Feliz.MaterialUI/blob/master/docs-app/public/pages/samples/sign-in/SignIn.fs
+// - https://cmeeren.github.io/Feliz.MaterialUI/#usage/styling
+// - https://material-ui.com/styles/basics/
+
+let useStyles = Styles.makeStyles(fun styles theme ->
+  {|
+    rootDiv = styles.create [
+        style.backgroundColor "#333333"
+        style.height (length.percent 100)
+        style.padding 10
+        style.fontSize 16
+        style.color "#fff"
+        style.fontFamily "system-ui, -apple-system, BlinkMacSystemFont, Roboto, Helvetica, sans-serif"
     ]
+  |}
+)
+
+let app = React.functionComponent(fun (model, dispatch) ->
+    let classes = useStyles ()
+    Mui.themeProvider [
+        themeProvider.theme darkTheme
+        themeProvider.children [
+            Html.div [
+                prop.className classes.rootDiv
+                prop.children [
+                    Html.input [
+                        prop.type' "file"
+                        prop.multiple true
+                        prop.style [style.display.none]
+                    ]
+                    Html.div [
+                        prop.style [ style.fontSize 24 ]
+                        prop.children [
+                            Html.text "Drag & drop anywhere to open files or use the "
+                            Mui.button [
+                                button.variant.outlined
+                                button.children "file picker"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+)
+
+//let render model dispatch =
+
+let render (state: State) (dispatch: Msg -> unit) =
+  app (state, dispatch)
+
+// let getSample (key: string) =
+//   React.elmishComponent("SignIn", init, update, render, key)
