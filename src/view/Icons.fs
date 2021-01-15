466ec8e527ca957b98fcf850c772cf1971d06935
David Neuy <quantjump@gmail.com>
2021-01-15 22:42:23 +0100

    Add tab icon same line with tabs and vertical bar.

--- src/view/Icons.fs
+++ src/view/Icons.fs
@@ -1,8 +1,10 @@
 module Icons
 
+open Feliz
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.React
+open Feliz.MaterialUI
 
 let inline gitHubIcon b : ReactElement = 
   ofImport "default" "@material-ui/icons/GitHub" (keyValueList CaseRules.LowerFirst b) []
@@ -12,3 +14,19 @@ let inline wrapTextIcon b : ReactElement =
 
 let inline AddIcon b : ReactElement = 
   ofImport "default" "@material-ui/icons/Add" (keyValueList CaseRules.LowerFirst b) []
+
+let inline VerticalBar b: ReactElement =
+  Mui.svgIcon [
+    prop.stroke color.white
+    prop.strokeWidth 1
+    prop.viewBox (0, 0, 3, 50)
+    prop.style [style.height 38; style.width 10; style.margin.auto; style.marginLeft 0; style.marginRight 0]
+    prop.children [
+      Svg.line [
+        Interop.svgAttribute "x1" "50%"
+        Interop.svgAttribute "y1" "15%"
+        Interop.svgAttribute "x2" "50%"
+        Interop.svgAttribute "y2" "75%"
+      ]
+    ]
+  ]
--- src/view/Tabbar.fs
+++ src/view/Tabbar.fs
@@ -13,23 +13,30 @@ let tabBarElement model dispatch (classes: CssClasses) =
       Mui.paper [
         prop.style [style.flexGrow 1; style.marginTop 5]
         paper.children [
-          Mui.tabs [
-            tabs.variant.scrollable
-            tabs.value model.SelectedTabId
-            tabs.onChange (TabChanged >> dispatch)
-            tabs.indicatorColor.primary
-            tabs.textColor.primary
-            tabs.children [
-              for t in model.TabItems ->
-                Mui.tab [
-                  tab.label t.Name
+          Html.div [
+            prop.style [style.display.flex]
+            prop.children [
+              Mui.tabs [
+                tabs.variant.scrollable
+                tabs.value model.SelectedTabId
+                tabs.onChange (TabChanged >> dispatch)
+                tabs.indicatorColor.primary
+                tabs.textColor.primary
+                tabs.children [
+                  for t in model.TabItems ->
+                    Mui.tab [
+                      tab.label t.Name
+                    ]
                 ]
+              ]
+              Icons.VerticalBar [
+              ]
+              Mui.iconButton [ 
+                prop.style [style.height 38; style.width 38; style.margin.auto; style.marginLeft 1; style.marginRight 5]
+                prop.onClick (fun _ -> dispatch AddEmptyTab)
+                iconButton.children (Icons.AddIcon []) 
+              ]
             ]
-          ]
-          Mui.iconButton [ 
-            prop.style [style.height 38; style.width 38; style.marginLeft 10]
-            prop.onClick (fun _ -> dispatch AddEmptyTab)
-            iconButton.children (Icons.AddIcon []) 
           ]
         ]
       ]
